782d5b32ba0efa937788eddb371b0da6ba8443de
MuhammadHasher017 <69762654+MuhammadHasher017@users.noreply.github.com>
2022-01-21 04:03:16 +0500

    final commit today

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,44 @@
-import { Component, OnInit, ViewChild ,ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { sampleData } from './datasource';
-import { ColumnMenuOpenEventArgs, PageSettingsModel,SortSettingsModel } from '@syncfusion/ej2-angular-grids';
+import {
+  ColumnMenuOpenEventArgs,
+  PageSettingsModel,
+  SortSettingsModel,
+} from '@syncfusion/ej2-angular-grids';
 import { WebsocketServiceService } from './websocket-service.service';
-import { EditSettingsModel, ToolbarItems, TreeGridComponent } from '@syncfusion/ej2-angular-treegrid';
-import { MenuItemModel , ContextMenu } from '@syncfusion/ej2-angular-navigations';
+import {
+  EditSettingsModel,
+  ToolbarItems,
+  TreeGridComponent,
+} from '@syncfusion/ej2-angular-treegrid';
+import {
+  MenuItemModel,
+  ContextMenu,
+} from '@syncfusion/ej2-angular-navigations';
 import { enableRipple } from '@syncfusion/ej2-base';
 import { getValue, isNullOrUndefined } from '@syncfusion/ej2-base';
 import { BeforeOpenCloseEventArgs } from '@syncfusion/ej2-inputs';
 import { MenuEventArgs } from '@syncfusion/ej2-navigations';
 import { DialogComponent } from '@syncfusion/ej2-angular-popups';
 import { EmitType } from '@syncfusion/ej2-base';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   // template: `` ,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'test-task-FE';
+    buttonName: any;
+
   public editSettings: EditSettingsModel;
   public toolbarOptions: ToolbarItems[];
   public data: Object[];
@@ -30,13 +47,12 @@ export class AppComponent implements OnInit{
   public contextMenuItems: Object[];
   @ViewChild('treegrid')
   public treeGridObj: TreeGridComponent;
-  public closeOnEscape: boolean =false;
-
-
+  public closeOnEscape: boolean = false;
 
   @ViewChild('ejDialog') ejDialog: DialogComponent;
   // Create element reference for dialog target element.
-  @ViewChild('container', { read: ElementRef, static: true }) container: ElementRef;
+  @ViewChild('container', { read: ElementRef, static: true })
+  container: ElementRef;
   // The Dialog shows within the target element.
   public targetElement: HTMLElement;
 
@@ -44,120 +60,208 @@ export class AppComponent implements OnInit{
   public dialogObj: DialogComponent;
   public form: FormGroup;
 
-
-
-
-
-
-  constructor(private websocketServiceService:WebsocketServiceService ,public formBuilder: FormBuilder){
-
-  }
+  constructor(
+    private websocketServiceService: WebsocketServiceService,
+    public formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
 
+
     this.form = this.formBuilder.group({
-      name: [null, Validators.required],
-      email: [null, Validators.required],
-      contact: [null, Validators.required],
-      upload: [null, Validators.required],
+      taskID: [null, Validators.required],
+      taskName: [null, Validators.required],
+      startDate: [null, Validators.required],
+      duration: [null, Validators.required],
     });
-    console.log("here");
+    console.log('here');
+
+
     this.initilaizeTarget();
-      this.websocketServiceService.listen("test event").subscribe((data : [])=>{
 
-        this.data = data;
 
-        console.log("here", data)
-      })
-      this.editSettings = {allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog'};
-      this.toolbarOptions = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
-      this.sortSettings = { columns: [{ field: 'taskName', direction: 'Ascending' }, { field: 'taskID', direction: 'Descending' }]  };
-      // this.contextMenuItems = ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',  'Edit',                     'Delete', 'Save', 'Cancel', 'PdfExport', 'ExcelExport', 'CsvExport'            ,                 'FirstPage', 'PrevPage', 'LastPage', 'NextPage'];
-      
-      this.contextMenuItems =  [
-        { text: 'Collapse the Row', target: '.e-content', id: 'collapserow'},
-        { text: 'Expand the Row', target: '.e-content', id: 'expandrow'},
-        { text: 'Insert', target: '.e-content', id: 'Insert', click: this.columnMenuOpen(3)},
-        { text: 'Insert', target: '.e-headercontent', id: 'Insert1' },
-        { text: 'Delete', target: '.e-headercontent', id: 'Delete2', click: this.columnMenuOpen(3) }
+    this.websocketServiceService.listen('test event').subscribe((data: []) => {
+      this.data = data;
+    });
+
+
+    this.editSettings = {
+      allowEditing: true,
+      allowAdding: true,
+      allowDeleting: true,
+      // mode: 'Dialog',
+    };
+
+    this.toolbarOptions = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+
+    this.sortSettings = {
+      columns: [
+        { field: 'taskName', direction: 'Ascending' },
+        { field: 'taskID', direction: 'Ascending' },
+      ],
+    };
+
+    this.contextMenuItems = [
+
+      { text: 'AddNext', target: '.e-content', id: 'addnext' },
+      { text: 'AddChild', target: '.e-content', id: 'addchild' },
+      { text: 'DelRow', target: '.e-content', id: 'delrow' },
+      { text: 'EditRow', target: '.e-content', id: 'editrow' },
+      { text: 'MultiSelect', target: '.e-content', id: 'multiselect' },
+      { text: 'CopyRows', target: '.e-content', id: 'copyrows' },
+      { text: 'CutRows', target: '.e-content', id: 'cutrows' },
+      { text: 'PasteNext', target: '.e-content', id: 'pastenext' },
+      { text: 'PasteChild', target: '.e-content', id: 'pastechild' },
+
+
+
+      { text: 'EditCol', target: '.e-headercontent', id: 'editcol' },
+      { text: 'NewCol', target: '.e-headercontent', id: 'newcol' },
+      { text: 'DelCol', target: '.e-headercontent', id: 'delcol' },
+      { text: 'ChooseCol', target: '.e-headercontent', id: 'choosecol' },
+      { text: 'FreezeCol', target: '.e-headercontent', id: 'freezecol' },
+      { text: 'FilterCol', target: '.e-headercontent', id: 'filtercol' },
+      { text: 'MultiSort', target: '.e-headercontent', id: 'multisort' },
+
+
+
     ];
-      
-      this.pageSettings = { pageSize: 6 };
-  
-    }
 
-    validateAllFormFields(formGroup: FormGroup) {
-      Object.keys(formGroup.controls).forEach(field => {
-        const control = formGroup.get(field);
-        if (control instanceof FormControl) {
-          control.markAsTouched({ onlySelf: true });
-        } else if (control instanceof FormGroup) {
-          this.validateAllFormFields(control);
-        }
-      });
-    }
-
-    contextMenuClick(args?: MenuEventArgs): void {
-      console.log("here", args);
-      let abc = args
-      this.treeGridObj.getColumnByField('taskID');
-      if (args.item.id === 'Insert1') {
-        console.log("insert call",abc);
-        
-          this.treeGridObj.collapseRow(<HTMLTableRowElement>(this.treeGridObj.getSelectedRows()[0]));
-      } 
-      else if (args.item.id === 'Insert') {
-        console.log("insert call",abc['rowInfo']['rowData']);
-        
-          this.treeGridObj.collapseRow(<HTMLTableRowElement>(this.treeGridObj.getSelectedRows()[0]));
-      } else {
-          this.treeGridObj.expandRow(<HTMLTableRowElement>(this.treeGridObj.getSelectedRows()[0]));
-          }
+    this.pageSettings = { pageSize: 10};
   }
 
-  columnMenuOpen(arg1){
-  console.log("sadsadasdasd");
-      
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+
+  contextMenuClick(args?: MenuEventArgs): void {
+
+    console.log('here', args);
+
+    let rowinfo = args['rowInfo']['rowData']
+
+    // let check =  this.treeGridObj.getColumnByField('taskID');
+    // console.log("check",check);
+    // this.treeGridObj.collapseRow(
+    //   <HTMLTableRowElement>this.treeGridObj.getSelectedRows()[0]
+    // );
+    
+  if (args.item.id === 'addnext') {
+      console.log('addnext');
+      this.buttonName = "Save"
+
+    } 
+    else if (args.item.id === 'addchild') {
+      this.buttonName = "Save"
+
+      console.log('addchild call');
+    } 
+    else if (args.item.id === 'delrow') {
+
+      console.log('delrow call');
+
+    }    
+    else if (args.item.id === 'editrow') {
+
+      this.formPatchValue(rowinfo)
+      this.buttonName = "Update"
+      console.log('editrow call');
+
+    }    
+    else if (args.item.id === 'multiselect') {
+
+      console.log('multiselect call');
+
+    }    
+    else if (args.item.id === 'copyrows') {
+
+      console.log('copyrows call');
+
+    }    
+    else if (args.item.id === 'cutrows') {
+
+      console.log('cutrows call');
+
+    } 
+    else if (args.item.id === 'pastenext') {
+
+      console.log('pastenext call');
+
+    } 
+    else if (args.item.id === 'pastechild') {
+
+      console.log('pastechild call');
+
+    } 
+    else {
 
     }
-    columnMenuClick(arg2?: MenuEventArgs){
-      console.log(arg2);
+  }
 
-    }
+  formPatchValue(value:any){
 
+    
+    this.form.patchValue({
+    taskID: value.taskID,
+    taskName:value.taskName,
+    startDate: new Date(value.startDate),
+    duration: value.duration,
+   })
+  //Date is still not patched
+   console.log( new Date(value.startDate));
+       this.onOpenDialog()
+  }
+  
 
-      // Initialize the Dialog component's target element.
+  // Initialize the Dialog component's target element.
   public initilaizeTarget: EmitType<object> = () => {
     this.targetElement = this.container.nativeElement.parentElement;
-      }
+  };
+
+  // Hide the Dialog when click the footer button.
+  public hideDialog: EmitType<object> = () => {
+
+    console.log("here");
     
-      // Hide the Dialog when click the footer button.
-      public hideDialog: EmitType<object> = () => {
-      this.ejDialog.hide();
-      }
-    
-      // Enables the footer buttons
-      public buttons: Object = [
+    this.ejDialog.hide();
+  };
+
+  // Enables the footer buttons
+  public buttons: Object = [
     {
-      'click': this.hideDialog.bind(this),
+      click: this.saveData(),
       // Accessing button component properties by buttonModel property
-        buttonModel:{
-        content: 'OK',
+      buttonModel: {
+        content: "Save",
         // Enables the primary button
-        isPrimary: true
-      }
+        isPrimary: true,
+      },
     },
     {
-      'click': this.hideDialog.bind(this),
+      click: this.hideDialog.bind(this),
       buttonModel: {
-        content: 'Cancel'
-      }
-    }
-      ];
-    
-      // Sample level code to handle the button click action
-      public onOpenDialog = function(event: any): void {
-      // Call the show method to open the Dialog
-      this.ejDialog.show();
-      };
+        content: 'Cancel',
+      },
+    },
+  ];
+
+  // Sample level code to handle the button click action
+  public onOpenDialog = function (): void {
+    // Call the show method to open the Dialog
+    this.ejDialog.show();
+  };
+
+  saveData(){
+
+    this.hideDialog.bind(this)
+  }
+  
 }
